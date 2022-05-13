e514ceae7b9a6d0ded69f3655f2f13df3b6ab0cd
Jose Vargas <sloty00>
2022-05-13 18:38:25 -0400

    Panel Administracion (firebase pendiente)

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,12 +1,13 @@
 const conexion = require('../database/db')
 
-exports.register = async (req, res)=>{
+exports.registerAdmin = async (req, res)=>{
     try {
         const user = req.body.user
         const pass = req.body.pass
         const type = 1
+
         if (user && pass) {
-            conexion.query('INSERT INTO tbl_acceso SET ?', {a_cuenta:user, a_password:pass, fk_tipo:type}, (error, result)=>{
+            conexion.query('INSERT INTO tbl_admin SET ?', {ad_cuenta:user, ad_password:pass, fk_tipo:type}, (error, result)=>{
                 if(error){
                     console.log(error);
                 }else{
@@ -37,14 +38,14 @@ exports.register = async (req, res)=>{
     }
 }
 
-exports.login = async (req, res)=>{
+exports.loginAdmin = async (req, res)=>{
     try {
         const user = req.body.user
         const pass = req.body.pass
 
         if (user && pass) {
             // Execute SQL query that'll select the account from the database based on the specified username and password
-            conexion.query('SELECT * FROM tbl_acceso WHERE a_cuenta = ? AND a_password = ?', [user, pass], function(error, result) {
+            conexion.query('SELECT * FROM tbl_admin WHERE ad_cuenta = ? AND ad_password = ?', [user, pass], function(error, result) {
                 // If there is an issue with the query, output the error
                 if (error) throw error;
                 // If the account exists
@@ -53,7 +54,7 @@ exports.login = async (req, res)=>{
                     req.session.loggedIn = true;
                     req.session.user = user;
                     // Redirect to home page
-                    res.render('index', {
+                    res.render('panelAdmin', {
                         'usuario':user, 
                         'password':pass,
                         alert:true,
@@ -64,26 +65,26 @@ exports.login = async (req, res)=>{
                         timer: false,
                     })
                 } else {
-                    res.render('login',{
+                    res.render('loginAdmin',{
                         alert:true,
                         alertTitle: "Advertencia",
                         alertMessage: "Datos Invalidos",
                         alertIcon:'warning',
                         showConfirmButton: true,
                         timer: false,
-                        ruta: 'login'
+                        ruta: 'loginAdmin'
                     })
                 }		
             });
         } else {
-            res.render('login',{
+            res.render('loginAdmin',{
                 alert:true,
                 alertTitle: "Advertencia",
                 alertMessage: "Ingrese un usuario y password",
                 alertIcon:'warning',
                 showConfirmButton: true,
                 timer: false,
-                ruta: 'login'
+                ruta: 'loginAdmin'
             })
         }
     } catch (error) {
@@ -91,19 +92,19 @@ exports.login = async (req, res)=>{
     }
 }
 
-exports.protected = async (req, res)=>{
+exports.protectedAdmin = async (req, res)=>{
     // If the user is loggedin
 	if (req.session.loggedIn) {
 		// Output username
-		res.render('index')
+		res.render('panelAdmin')
 	} else {
 		// Not logged in
-		res.redirect('/login')
+		res.redirect('/loginAdmin')
 	}
 	res.end();
 }
 
-exports.logout = (req, res)=>{
+exports.logoutAdmin = (req, res)=>{
     req.session.destroy((err)=>{})
-    return res.redirect('/')
+    return res.redirect('/loginAdmin')
 }
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 const express  = require('express')
 const router = express.Router()
 const authController = require('../controller/authController');
+const adminController = require('../controller/adminController');
 
 //router para las vistas
 router.get('/', authController.protected, (req, res)=>{    
@@ -12,12 +13,29 @@ router.get('/login', (req, res)=>{
 })
 
 router.get('/register', (req, res)=>{
-    res.render('register')
+    res.render('register',{alert:false});
 })
+
+router.get('/registerAdmin', (req, res)=>{
+    res.render('registerAdmin',{alert:false})
+})
+
+router.get('/loginAdmin', (req, res)=>{
+    res.render('loginAdmin', {alert:false})
+})
+
+router.get('/panelAdmin', authController.protected, (req, res)=>{    
+    res.render('panelAdmin')
+})
+
 
 //router para los metodos del controller
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
+router.post('/registerAdmin', adminController.registerAdmin);
+router.post('/loginAdmin', adminController.loginAdmin);
+router.get('/logoutAdmin', adminController.logoutAdmin);
+
 
 module.exports = router
